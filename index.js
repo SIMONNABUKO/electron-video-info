07f0ccbeb5cfaa202fa2dfd2012012bed15f5359
Simon Angatia <simonnabuko@gmail.com>
2019-09-21 08:36:56 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const electron = require('electron');
+const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+let mainWindow;
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+      mainWindow.webContents.send('video:meatadata', metadata.format.duration);
+    });
+  });
+});
